c171898111e7835f734deabeeadd0167d6295c07
Asilbekrasul <96375724+Asilbekrasul@users.noreply.github.com>
2022-01-16 17:34:39 +0300

    login page

---
+++ app.js
@@ -0,0 +1,12 @@
+{/* <script>
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings'
+  });
+</script> */}
+
+
+
+var typed = new Typed('.element', {
+    strings: ['This is a JavaScript library', 'This is an ES6 module'],
+    smartBackspace: true // Default value
+  });
---
+++ vendors/app.js
@@ -0,0 +1,10 @@
+let option = {
+    strings: [
+        'ASSALOMU ALAYKUM. ','BIZNING WEB SAXIFAMIZGA  XUSH KELIBSIZ.','BIZNI TANLAGANINGIZ UCHUN RAXMAT.'
+    ],
+    typeSpeed: 60,
+    backSpeed: 50,
+    backDelay: 2000,
+    
+}
+let typed = new Typed('.element',option);
